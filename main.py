dddb57cdf859b43c6d782ed429b5414eb8307cfd
Rocky Sokolovas <36315873+rocky-sokolovas@users.noreply.github.com>
2025-04-09 22:35:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import randint,choice,shuffle
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -8,21 +8,18 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 def gen_password():
-    nr_letters = random.randint(8, 10) 
-    nr_symbols = random.randint(2, 4)
-    nr_numbers = random.randint(2, 4)
+    nr_letters = randint(8, 10) 
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
 
-    pass_letters=[random.choice(letters) for _ in range(nr_letters)]
-    pass_symbols=[random.choice(symbols) for _ in range(nr_symbols)]
-    pass_numbers=[random.choice(numbers) for _ in range(nr_numbers)]
+    pass_letters=[choice(letters) for _ in range(nr_letters)]
+    pass_symbols=[choice(symbols) for _ in range(nr_symbols)]
+    pass_numbers=[choice(numbers) for _ in range(nr_numbers)]
 
     password_list=pass_letters+ pass_numbers+pass_symbols
 
-    random.shuffle(password_list)
-
-    password = ""
-    for char in password_list:
-      password += char
+    shuffle(password_list)
+    password="".join(password_list)
 
     pass_entry.insert(0,password)
 
